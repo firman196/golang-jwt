77bcb3127eb988a022205280e2e631390cb342e3
firmandev196 <firman.agus@sirclo.com>
2023-02-16 20:57:50 +0700

    finishing repository

--- repository/users_repository.go
+++ repository/users_repository.go
@@ -9,7 +9,7 @@ import (
 type UsersRepository interface {
 	Create(ctx context.Context, tx *sql.Tx, user entity.Users) entity.Users
 	Update(ctx context.Context, tx *sql.Tx, user entity.Users) entity.Users
-	Delete(ctx context.Context, tx *sql.Tx, user entity.Users) bool
+	Delete(ctx context.Context, tx *sql.Tx, userId int16) bool
 	GetById(ctx context.Context, tx *sql.Tx, userId int16) (entity.Users, error)
 	GetByEmail(ctx context.Context, tx *sql.Tx, email string) (entity.Users, error)
 	GetAll(ctx context.Context, tx *sql.Tx) []entity.Users
--- repository/users_repository_impl.go
+++ repository/users_repository_impl.go
@@ -3,15 +3,16 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"golang-jwt/helper"
 	"golang-jwt/model/entity"
 )
 
 type UsersRepositoryImpl struct{}
 
-//func NewUsersRepositoryImpl() UsersRepository {
-//	return &UsersRepository{}
-//}
+func NewUsersRepositoryImpl() UsersRepository {
+	return &UsersRepositoryImpl{}
+}
 
 func (repository *UsersRepositoryImpl) Create(ctx context.Context, tx *sql.Tx, user entity.Users) entity.Users {
 	SQL := "INSERT INTO users (id, first_name, last_name, email, password) VALUES (?,?,?,?,?)"
@@ -58,14 +59,85 @@ func (repository *UsersRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, u
 	return true
 }
 
-func (repository *UsersRepositoryImpl) GetById(ctx context.Context, tx *sql.Tx, userId int16) entity.Users {
+func (repository *UsersRepositoryImpl) GetById(ctx context.Context, tx *sql.Tx, userId int16) (entity.Users, error) {
 	SQL := "SELECT id, first_name, last_name, email FROM users WHERE id = ?"
-	_, err := tx.ExecContext(
+	rows, err := tx.QueryContext(
 		ctx,
 		SQL,
 		userId,
 	)
 
 	helper.SetPanicError(err)
-	return entity.Users{}
+	defer rows.Close()
+
+	user := entity.Users{}
+	if rows.Next() {
+		err := rows.Scan(
+			&user.Id,
+			&user.Firstname,
+			&user.Lastname,
+			&user.Email,
+		)
+
+		helper.SetPanicError(err)
+		return user, nil
+	} else {
+		return user, errors.New("user not found")
+	}
+}
+
+func (repository UsersRepositoryImpl) GetByEmail(ctx context.Context, tx *sql.Tx, email string) (entity.Users, error) {
+	SQL := "SELECT id, first_name, last_name, email FROM users WHERE email = ?"
+
+	rows, err := tx.QueryContext(
+		ctx,
+		SQL,
+		email,
+	)
+
+	helper.SetPanicError(err)
+	defer rows.Close()
+
+	user := entity.Users{}
+	if rows.Next() {
+		err := rows.Scan(
+			&user.Id,
+			&user.Firstname,
+			&user.Lastname,
+			&user.Email,
+		)
+		helper.SetPanicError(err)
+
+		return user, nil
+	} else {
+		return user, errors.New("user not found")
+	}
+}
+
+func (repository *UsersRepositoryImpl) GetAll(ctx context.Context, tx *sql.Tx) []entity.Users {
+	SQL := "SELECT id, first_name, last_name, email FROM users"
+	rows, err := tx.QueryContext(
+		ctx,
+		SQL,
+	)
+
+	helper.SetPanicError(err)
+	defer rows.Close()
+
+	var users []entity.Users
+	for rows.Next() {
+		user := entity.Users{}
+		err := rows.Scan(
+			&user.Id,
+			&user.Firstname,
+			&user.Lastname,
+			&user.Email,
+		)
+		helper.SetPanicError(err)
+
+		users = append(users, user)
+	}
+
+	return users
+
 }
