eac2a04220379692de284512e67108e26890c0b7
firmandev196 <firman.agus@sirclo.com>
2023-03-05 19:45:18 +0700

    add auth middleware

--- exception/api/error_handler.go
+++ exception/api/error_handler.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// ---- Bad Request Err ----//
 type BadRequestError struct {
 	Error string
 }
@@ -16,12 +17,31 @@ func NewBadRequestError(error string) BadRequestError {
 	}
 }
 
+//---- End Bad Request Err ----//
+
+// ---- Unautorized Request Err ----//
+type UnautorizedRequestError struct {
+	Error string
+}
+
+func NewUnautorizedRequestError(error string) UnautorizedRequestError {
+	return UnautorizedRequestError{
+		Error: error,
+	}
+}
+
+//---- End Unautorized Request Err ----//
+
 func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interface{}) {
 	if badRequest(writer, request, err) {
 		return
 	}
 
 	if notFound(writer, request, err) {
+		return
+	}
+
+	if unauthorized(writer, request, err) {
 		return
 	}
 
@@ -75,4 +95,22 @@ func internalServerError(writer http.ResponseWriter, request *http.Request, erro
 	}
 
 	helper.JsonEncode(writer, response)
+}
+
+func unauthorized(writer http.ResponseWriter, request *http.Request, error interface{}) bool {
+	exception, err := error.(UnautorizedRequestError)
+	if err {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+		response := web.GeneralResponse{
+			Status: "Unautorized",
+			Data:   exception.Error,
+		}
+
+		helper.JsonEncode(writer, response)
+		return true
+	} else {
+		return false
+	}
+
 }
---
+++ middleware/auth_middleware.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	exception "golang-jwt/exception/api"
+	"golang-jwt/model/web/token"
+	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{
+		Handler: handler,
+	}
+}
+
+func (middleware *AuthMiddleware) ServerHttp(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" && (request.RequestURI == "/api/v1/user" || request.RequestURI == "/api/v1/auth") {
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
+		tokenAuth := request.Header.Get("Authorization")
+		if tokenAuth == "" {
+			panic(exception.NewUnautorizedRequestError("User Unautorized"))
+		}
+
+		var jwtTokenSecret = []byte(os.Getenv("JWT_TOKEN_SECRET"))
+
+		claims := &token.TokenClaims{}
+
+		token, err := jwt.ParseWithClaims(tokenAuth, claims, func(t *jwt.Token) (interface{}, error) {
+			return jwtTokenSecret, nil
+		})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				panic(exception.NewUnautorizedRequestError(err.Error()))
+			}
+		}
+
+		if !token.Valid {
+			panic(exception.NewUnautorizedRequestError(err.Error()))
+		}
+
+		middleware.Handler.ServeHTTP(writer, request)
+	}
+
+}
